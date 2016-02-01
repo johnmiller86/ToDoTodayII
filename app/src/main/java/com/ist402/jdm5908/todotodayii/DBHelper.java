89c4af7630f15e22f07649624b3e9df9f30629af
johnmiller86 <chefjohn2006@hotmail.com>
2016-02-01 12:16:28 -0500

    Initial commit

--- app/src/main/java/com/ist402/jdm5908/todotodayii/DBHelper.java
+++ app/src/main/java/com/ist402/jdm5908/todotodayii/DBHelper.java
@@ -1,4 +1,56 @@
 package com.ist402.jdm5908.todotodayii;
 
-public class DBHelper {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DBHelper extends SQLiteOpenHelper{
+
+    // Defining the database and table
+    private static final int DATABASE_VERSION = 1;
+    private static final String DATABASE_NAME = "toDo_Today";
+    private static final String DATABASE_TABLE = "toDo_Items";
+
+    // Defining the column names for the table
+    private static final String KEY_TASK_ID = "_id";
+    private static final String KEY_DESCRIPTION = "description";
+    private static final String KEY_IS_DONE = "is_done";
+
+    private int taskCount;
+
+    public DBHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db){
+
+        String table =
+                "CREATE TABLE " + DATABASE_TABLE + "("
+                + KEY_TASK_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + KEY_DESCRIPTION + " TEXT, "
+                + KEY_IS_DONE + " INTEGER" + ")";
+        db.execSQL(table);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase database, int oldVersion, int newVersion){
+
+        // Drop old table if exists
+        database.execSQL("DROP TABLE IF EXISTS " + DATABASE_TABLE);
+
+        // Recreate table
+        onCreate(database);
+    }
+
+    //************************ DATABASE OPERATIONS: ADD, EDIT, DELETE
+    // Adding new task
+    public void addToDoItem(ToDo_Item task){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        // Add key-value pair information for the task descriptoin
+        values.put(KEY_DESCRIPTION, task.getDescription());
+    }
 }
