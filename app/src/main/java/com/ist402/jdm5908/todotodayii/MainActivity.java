944c1c5ae9658c2e2fc41ba6cd5c2203c3b58997
johnmiller86 <chefjohn2006@hotmail.com>
2016-02-01 13:15:56 -0500

    Initial commit

--- app/src/main/java/com/ist402/jdm5908/todotodayii/MainActivity.java
+++ app/src/main/java/com/ist402/jdm5908/todotodayii/MainActivity.java
@@ -3,7 +3,7 @@ package com.ist402.jdm5908.todotodayii;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-// Additional generic cop compliment, Brian....
+// Additional generic cop compliment, Brian...
 
 public class MainActivity extends AppCompatActivity {
 
