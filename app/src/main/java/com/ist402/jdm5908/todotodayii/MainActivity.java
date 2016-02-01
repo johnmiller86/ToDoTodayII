23673bf9bf25686c41c96512ec84433ab2e89d0a
johnmiller86 <chefjohn2006@hotmail.com>
2016-02-01 00:37:45 -0500

    Initial commit

---
+++ app/src/main/java/com/ist402/jdm5908/todotodayii/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ist402.jdm5908.todotodayii;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/ist402/jdm5908/todotodayii/ToDo_Item.java
@@ -0,0 +1,4 @@
+package com.ist402.jdm5908.todotodayii;
+
+public class ToDo_Item {
+}
